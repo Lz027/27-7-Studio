7cde71485f2615c591d7de80ef08e55c18b12a32
Ahmed Baghni <161574715+Lz027@users.noreply.github.com>
2026-07-27 16:42:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tanstackStart({
-      server: { entry: "server" },
-    }),
-    react(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
